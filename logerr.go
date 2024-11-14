79545119f0d70412657c22931afa99292a8ca257
Wouter Kuijper <248280+wkuijper@users.noreply.github.com>
2024-11-14 08:25:46 +0100

    Additional logging functions

--- logerr.go
+++ logerr.go
@@ -1,5 +1,21 @@
 package logerr
 
+import (
+	"io"
+)
+
+type LogSink interface {
+	Log(level string, format string, args ...any)
+}
+
 func F(format string, args ...any) {
+	// DISABLED
+}
+
+func FF(out io.Writer, format string, args ...any) {
+	// DISABLED
+}
+
+func SF(sink LogSink, format string, args ...any) {
 	// DISABLED
 }
